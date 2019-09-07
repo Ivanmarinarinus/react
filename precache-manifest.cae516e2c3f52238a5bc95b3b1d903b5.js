8ad78ae5e918ede03f531f827608d6242d72dfc1
Ivanmarinarinus <51843224+Ivanmarinarinus@users.noreply.github.com>
2019-09-07 11:30:14 -0300

    Add files via upload

---
+++ precache-manifest.cae516e2c3f52238a5bc95b3b1d903b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44623cf5be03202511b28d8d310c9c18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7895a518b95938de5311",
+    "url": "/static/css/main.153c9676.chunk.css"
+  },
+  {
+    "revision": "c51c860545aca0adb137",
+    "url": "/static/js/2.0aaeb3f2.chunk.js"
+  },
+  {
+    "revision": "7895a518b95938de5311",
+    "url": "/static/js/main.b2e70d8a.chunk.js"
+  },
+  {
+    "revision": "6fa4799e4283bf5e0851",
+    "url": "/static/js/runtime~main.259a7d77.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  }
+]);
